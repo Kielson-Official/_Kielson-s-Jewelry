5b83eae0d3365c2e0b0a1ed403e69c3861fb3607
Kielson <gracjanprzymus12@gmail.com>
2026-08-09 12:02:40 +0200

    Ring item model generation

--- src/main/java/com/kielson/datagen/RingsAndThingsModelProvider.java
+++ src/main/java/com/kielson/datagen/RingsAndThingsModelProvider.java
@@ -1,10 +1,21 @@
 package com.kielson.datagen;
 
+import com.kielson.item.RingsAndThingsItems;
+import com.kielson.util.JewelryComponent;
+import com.kielson.util.RingsAndThingsComponents;
 import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.minecraft.client.data.models.BlockModelGenerators;
 import net.minecraft.client.data.models.ItemModelGenerators;
+import net.minecraft.client.data.models.model.ItemModelUtils;
+import net.minecraft.client.data.models.model.ModelTemplates;
+import net.minecraft.client.data.models.model.TextureMapping;
+import net.minecraft.client.renderer.item.properties.select.ComponentContents;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.item.Item;
 import org.jspecify.annotations.NonNull;
+
+import java.util.List;
 
 public class RingsAndThingsModelProvider extends FabricModelProvider {
     public RingsAndThingsModelProvider(FabricPackOutput output) {
@@ -18,6 +29,57 @@ public class RingsAndThingsModelProvider extends FabricModelProvider {
 
     @Override
     public void generateItemModels(@NonNull ItemModelGenerators itemModelGenerators) {
+        generateJewelryItem(RingsAndThingsItems.STONE_RING, itemModelGenerators);
+        generateJewelryItem(RingsAndThingsItems.COPPER_RING, itemModelGenerators);
+        generateJewelryItem(RingsAndThingsItems.IRON_RING, itemModelGenerators);
+        generateJewelryItem(RingsAndThingsItems.GOLDEN_RING, itemModelGenerators);
+        generateJewelryItem(RingsAndThingsItems.NETHERITE_RING, itemModelGenerators);
+    }
 
+    private void generateJewelryItem(Item item, ItemModelGenerators itemModelGenerators){
+        itemModelGenerators.itemModelOutput.accept(item, ItemModelUtils.select(
+                new ComponentContents<>(RingsAndThingsComponents.JEWELRY_COMPONENT),
+                ItemModelUtils.when(new JewelryComponent("none"), ItemModelUtils.plainModel(itemModelGenerators.createFlatItemModel(
+                        item,
+                        "_empty",
+                        ModelTemplates.FLAT_ITEM))),
+                ItemModelUtils.when(new JewelryComponent("diamond"), ItemModelUtils.plainModel(itemModelGenerators.generateLayeredItem(
+                        TextureMapping.getItemTexture(item, "_diamond").sprite(),
+                        TextureMapping.getItemTexture(item),
+                        TextureMapping.getItemTexture(item, "_diamond")))),
+                ItemModelUtils.when(new JewelryComponent("emerald"), ItemModelUtils.plainModel(itemModelGenerators.generateLayeredItem(
+                        TextureMapping.getItemTexture(item, "_emerald").sprite(),
+                        TextureMapping.getItemTexture(item),
+                        TextureMapping.getItemTexture(item, "_emerald")))),
+                ItemModelUtils.when(new JewelryComponent("amethyst"), ItemModelUtils.plainModel(itemModelGenerators.generateLayeredItem(
+                        TextureMapping.getItemTexture(item, "_amethyst").sprite(),
+                        TextureMapping.getItemTexture(item),
+                        TextureMapping.getItemTexture(item, "_amethyst")))),
+                ItemModelUtils.when(new JewelryComponent("resin"), ItemModelUtils.plainModel(itemModelGenerators.generateLayeredItem(
+                        TextureMapping.getItemTexture(item, "_resin").sprite(),
+                        TextureMapping.getItemTexture(item),
+                        TextureMapping.getItemTexture(item, "_resin")))),
+                ItemModelUtils.when(new JewelryComponent("prismarine"), ItemModelUtils.plainModel(itemModelGenerators.generateLayeredItem(
+                        TextureMapping.getItemTexture(item, "_prismarine").sprite(),
+                        TextureMapping.getItemTexture(item),
+                        TextureMapping.getItemTexture(item, "_prismarine")))),
+                ItemModelUtils.when(new JewelryComponent("obsidian"), ItemModelUtils.plainModel(itemModelGenerators.generateLayeredItem(
+                        TextureMapping.getItemTexture(item, "_obsidian").sprite(),
+                        TextureMapping.getItemTexture(item),
+                        TextureMapping.getItemTexture(item, "_obsidian")))),
+                ItemModelUtils.when(new JewelryComponent("quartz"), ItemModelUtils.plainModel(itemModelGenerators.generateLayeredItem(
+                        TextureMapping.getItemTexture(item, "_quartz").sprite(),
+                        TextureMapping.getItemTexture(item),
+                        TextureMapping.getItemTexture(item, "_quartz")))),
+                ItemModelUtils.when(new JewelryComponent("glowstone"), ItemModelUtils.plainModel(itemModelGenerators.generateLayeredItem(
+                        TextureMapping.getItemTexture(item, "_glowstone").sprite(),
+                        TextureMapping.getItemTexture(item),
+                        TextureMapping.getItemTexture(item, "_glowstone")))),
+                ItemModelUtils.when(new JewelryComponent("redstone"), ItemModelUtils.plainModel(itemModelGenerators.generateLayeredItem(
+                        TextureMapping.getItemTexture(item, "_redstone").sprite(),
+                        TextureMapping.getItemTexture(item),
+                        TextureMapping.getItemTexture(item, "_redstone"))))
+            )
+        );
     }
 }
